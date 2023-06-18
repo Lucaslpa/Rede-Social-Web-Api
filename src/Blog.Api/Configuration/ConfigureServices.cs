56475c648a7c0db5d0fef41120a248c12a4eb454
Lucas <lucaslpa12345@gmail.com>
2023-06-18 17:32:23 -0300

    movendo services configs para uma classe especifica

---
+++ src/Blog.Api/Configuration/ConfigureServices.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Data.database;
+
+namespace Blog.api.Configuration
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection Configure( this IServiceCollection Services, IConfiguration configuration )
+
+        {
+            // Add services to the container.
+            Services.AddNewtonSoftConfiguration();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            Services.AddEndpointsApiExplorer();
+            Services.AddSwaggerGen();
+            Services.AddDependencyInjection();
+            Services.AddAuthorization();
+            Services.AddDbContext<MyDatabaseContext>();
+            Services.AddIdentityConfiguration();
+            Services.AddAutoMapperProfiles();
+            Services.ConfigureJwt( configuration );
+            Services.AddCors();
+
+            return Services;
+        }
+    }
+}
--- src/Blog.Api/Program.cs
+++ src/Blog.Api/Program.cs
@@ -7,18 +7,7 @@ var builder = WebApplication.CreateBuilder( args );
 var Services = builder.Services;
 var configuration = builder.Configuration;
 
-// Add services to the container.
-Services.AddNewtonSoftConfiguration();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-Services.AddEndpointsApiExplorer();
-Services.AddSwaggerGen();
-Services.AddDependencyInjection();
-Services.AddAuthorization();
-Services.AddDbContext<MyDatabaseContext>();
-Services.AddIdentityConfiguration();
-Services.AddAutoMapperProfiles();
-Services.ConfigureJwt( configuration );
-
+Services.Configure( configuration );
 
 
 var app = builder.Build();
